fe586b7aa713d14b45d344afbe8d6e78c86c3a26
IsabelleAnn <isabelle.ann85@gmail.com>
2022-09-13 17:40:22 -0500

    Add background images, header image and continue page design

--- main.js
+++ main.js
@@ -1,1 +1,116 @@
-(()=>{"use strict";const e=document.getElementById("content"),t=document.createElement("div");t.classList.add("home-container"),e.appendChild(t);const n=document.createElement("div");n.classList.add("menu-container"),n.classList.add("hidden");const d=document.createElement("div");d.classList.add("contact-container"),d.classList.add("hidden");const i=document.querySelector("nav");!function(){const e=document.createElement("div");e.setAttribute("id","home"),e.setAttribute("class","navlink"),e.textContent="Home",e.addEventListener("click",(()=>{t.classList.remove("hidden"),n.classList.add("hidden"),d.classList.add("hidden")}));const c=document.createElement("div");c.setAttribute("id","menu"),c.setAttribute("class","navlink"),c.textContent="Menu",c.addEventListener("click",(()=>{t.classList.add("hidden"),n.classList.remove("hidden"),d.classList.add("hidden")}));const s=document.createElement("div");s.setAttribute("id","contact"),s.setAttribute("class","navlink"),s.textContent="Contact",s.addEventListener("click",(()=>{t.classList.add("hidden"),n.classList.add("hidden"),d.classList.remove("hidden")})),i.appendChild(e),i.appendChild(c),i.appendChild(s)}(),function(e,t){e.appendChild(t);const n=document.createElement("h1");n.textContent="Alamo",t.appendChild(n);const d=document.createElement("h3");d.textContent="Tex-Mex Restaurant",t.appendChild(d);const i=document.createElement("img");i.src="/src/images/logo.png",t.appendChild(i)}(e,t),function(e,t){e.appendChild(t);const n=document.createElement("h1");n.textContent="Menu",t.appendChild(n),[{itemName:"Fruit Bowl",itemDescription:"A small bowl of fresh fruit",itemPrice:"$3",itemImgSrc:"/src/images/logo.png"},{itemName:"Toast and Jam",itemDescription:"A slice of toast, your choice of bread, and our homemade blackberry or raspberry jam.",itemPrice:"$1",itemImgSrc:"/src/images/logo.png"}].forEach((e=>{const n=document.createElement("h3");n.textContent=e.itemName,n.classList.add("item-name"),t.appendChild(n);const d=document.createElement("p");d.textContent=e.itemDescription,d.classList.add("item-description"),t.appendChild(d);const i=document.createElement("h3");i.textContent=e.itemPrice,i.classList.add("item-price"),t.appendChild(i);const c=document.createElement("img");c.src=e.itemImgSrc,c.classList.add("item-img"),t.appendChild(c)}))}(e,n),function(e,t){e.appendChild(t);const n=document.createElement("h1");n.textContent="Contact",t.appendChild(n)}(e,d)})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/contact.js":
+/*!************************!*\
+  !*** ./src/contact.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"loadContactPage\": () => (/* binding */ loadContactPage)\n/* harmony export */ });\nfunction loadContactPage(container, contactContainer) {\n\n    container.appendChild(contactContainer);\n    const title = document.createElement('h1');\n    title.textContent = \"Contact\";\n    contactContainer.appendChild(title);\n}\n\n//# sourceURL=webpack://restaurant/./src/contact.js?");
+
+/***/ }),
+
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"loadHomePage\": () => (/* binding */ loadHomePage)\n/* harmony export */ });\nfunction loadHomePage(container, homeContainer) {\n\n\n    container.appendChild(homeContainer);\n    const title = document.createElement('h1');\n    title.textContent = 'Alamo';\n    homeContainer.appendChild(title);\n\n    const heading = document.createElement('h3');\n    heading.textContent = 'Tex-Mex Restaurant';\n    homeContainer.appendChild(heading);\n\n\n}\n\n//# sourceURL=webpack://restaurant/./src/home.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _home_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home.js */ \"./src/home.js\");\n/* harmony import */ var _menu_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./menu.js */ \"./src/menu.js\");\n/* harmony import */ var _contact_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./contact.js */ \"./src/contact.js\");\n\n\n\n\nconst header = document.querySelector('header');\nconst headerImg = document.createElement('img');\nheaderImg.src = 'images/Alamo-Logo-Web-Header.png';\nheaderImg.classList.add('header-img');\nheader.appendChild(headerImg);\n\nconst container = document.getElementById('content');\n\n//Creates Home Container\nconst homeContainer = document.createElement('div');\nhomeContainer.classList.add('home-container');\ncontainer.appendChild(homeContainer);\n\n//Creates Menu Container\nconst menuContainer = document.createElement('div');\nmenuContainer.classList.add('menu-container');\nmenuContainer.classList.add('hidden');\n\n//Creates Contact Container\nconst contactContainer = document.createElement('div');\ncontactContainer.classList.add('contact-container');\ncontactContainer.classList.add('hidden');\n\nconst nav = document.createElement('div');\nnav.classList.add('nav');\nheader.appendChild(nav);\n\ncreateNavbar();\n(0,_home_js__WEBPACK_IMPORTED_MODULE_0__.loadHomePage)(container, homeContainer);\n(0,_menu_js__WEBPACK_IMPORTED_MODULE_1__.loadMenuPage)(container, menuContainer);\n(0,_contact_js__WEBPACK_IMPORTED_MODULE_2__.loadContactPage)(container, contactContainer);\n\nfunction createNavbar() {\n    const homeBtn = document.createElement('div');\n    homeBtn.setAttribute('id', 'home');\n    homeBtn.setAttribute('class', 'navlink');\n    homeBtn.textContent = \"Home\";\n    homeBtn.addEventListener('click', () => {\n        homeContainer.classList.remove('hidden');\n        menuContainer.classList.add('hidden');\n        contactContainer.classList.add('hidden');\n    });\n\n    const menuBtn = document.createElement('div');\n    menuBtn.setAttribute('id', 'menu');\n    menuBtn.setAttribute('class', 'navlink');\n    menuBtn.textContent = \"Menu\";\n    menuBtn.addEventListener('click', () => {\n        homeContainer.classList.add('hidden');\n        menuContainer.classList.remove('hidden');\n        contactContainer.classList.add('hidden');\n    });\n\n    const contactBtn = document.createElement('div');\n    contactBtn.setAttribute('id', 'contact');\n    contactBtn.setAttribute('class', 'navlink');\n    contactBtn.textContent = \"Contact\";\n    contactBtn.addEventListener('click', () => {\n        homeContainer.classList.add('hidden');\n        menuContainer.classList.add('hidden');\n        contactContainer.classList.remove('hidden');\n    });\n\n    nav.appendChild(homeBtn);\n    nav.appendChild(menuBtn);\n    nav.appendChild(contactBtn);\n}\n\n//# sourceURL=webpack://restaurant/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/menu.js":
+/*!*********************!*\
+  !*** ./src/menu.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"loadMenuPage\": () => (/* binding */ loadMenuPage)\n/* harmony export */ });\nfunction loadMenuPage(container, menuContainer) {\n\n    container.appendChild(menuContainer);\n    const title = document.createElement('h1');\n    title.textContent = 'Menu';\n    menuContainer.appendChild(title);\n    const itemList = [{ itemName: 'Fruit Bowl', itemDescription: 'A small bowl of fresh fruit', itemPrice: '$3', itemImgSrc: 'images/logo.png' }, { itemName: 'Toast and Jam', itemDescription: 'A slice of toast, your choice of bread, and our homemade blackberry or raspberry jam.', itemPrice: '$1', itemImgSrc: 'images/logo.png' }];\n    itemList.forEach(item => {\n        const name = document.createElement('h3');\n        name.textContent = item.itemName;\n        name.classList.add('item-name');\n        menuContainer.appendChild(name);\n\n        const description = document.createElement('p');\n        description.textContent = item.itemDescription;\n        description.classList.add('item-description');\n        menuContainer.appendChild(description);\n\n        const price = document.createElement('h3');\n        price.textContent = item.itemPrice;\n        price.classList.add('item-price');\n        menuContainer.appendChild(price);\n\n        const image = document.createElement('img');\n        image.src = item.itemImgSrc;\n        image.classList.add('item-img');\n        menuContainer.appendChild(image);\n    });\n}\n\n//# sourceURL=webpack://restaurant/./src/menu.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
