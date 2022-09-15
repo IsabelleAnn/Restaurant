de82df0e5af2f117b52da0334383982479b1126a
IsabelleAnn <isabelle.ann85@gmail.com>
2022-09-15 14:52:40 -0500

    Add content to Contact Page, Add image slideshow to Home Page, Add link hover chili pepper image

--- src/contact.js
+++ src/contact.js
@@ -1,8 +1,41 @@
 export function loadContactPage(container, contactContainer) {
+    const textContainer = document.createElement('div');
+
+    const locationContainer = document.createElement('div');
+    const locationTitle = document.createElement('h1');
+    const locationDetails = document.createElement('p');
+
+    const contactTextContainer = document.createElement('div');
+    const contactTitle = document.createElement('h1');
+    const contactDetails = document.createElement('p');
+
+    const hoursContainer = document.createElement('div');
+    const hoursTitle = document.createElement('h1');
+    const hoursDetails = document.createElement('p');
+
+    locationTitle.textContent = 'Location';
+    locationDetails.innerHTML = 'Somewhere Down 4th Ln, <br /> San Antonio, TX 79456';
+    contactTitle.textContent = 'Contact Us';
+    contactDetails.innerHTML = 'Tel: 123-456-7890 <br /> Email: thealamo@sanantonio.com';
+    hoursTitle.textContent = 'Hours';
+    hoursDetails.innerHTML = 'Mon - Fri : 9AM - 9PM <br /> Sat - Sun: 9AM - 12AM';
+
+    textContainer.classList.add('contact-page-text-container');
+
+    locationContainer.classList.add('location-container');
+    contactTextContainer.classList.add('contact-text-container');
+    hoursContainer.classList.add('hours-container');
+
+    locationTitle.classList.add('page-title');
+    hoursTitle.classList.add('page-title');
+    contactTitle.classList.add('page-title');
 
     container.appendChild(contactContainer);
-    const title = document.createElement('h1');
-    title.textContent = "Contact";
-    title.classList.add('page-title');
-    contactContainer.appendChild(title);
+    contactContainer.appendChild(textContainer);
+    textContainer.appendChild(locationContainer);
+    textContainer.appendChild(contactTextContainer);
+    textContainer.appendChild(hoursContainer);
+    locationContainer.append(locationTitle, locationDetails);
+    contactTextContainer.append(contactTitle, contactDetails);
+    hoursContainer.append(hoursTitle, hoursDetails);
 }
--- src/home.js
+++ src/home.js
@@ -1,9 +1,42 @@
 export function loadHomePage(container, homeContainer) {
 
-    container.appendChild(homeContainer);
+    const buttonL = document.createElement('img');
+    buttonL.src = 'images/arrow.png';
+    buttonL.classList.add('left-arrow');
+    const buttonR = document.createElement('img');
+    buttonR.src = 'images/arrow.png';
+    buttonR.classList.add('right-arrow');
+
+    const backgroundImages = ['images/tacos.png', 'images/queso.png', 'images/mojo-pollo.jpg', 'images/tostadas.jpg', 'images/contact-background.jpg'];
+    let slideIndex = 0;
+    homeContainer.style.backgroundImage = `url(${backgroundImages[0]})`;
+    buttonL.addEventListener('click', slideImagesRightToLeft);
+    buttonR.addEventListener('click', slideImagesLeftToRight);
+
+    function slideImagesLeftToRight() {
+        if (slideIndex < backgroundImages.length - 1) {
+            slideIndex++;
+        } else {
+            slideIndex = 0;
+        }
+        homeContainer.style.backgroundImage = `url(${backgroundImages[slideIndex]})`;
+    }
+
+    function slideImagesRightToLeft() {
+        if (slideIndex > 0) {
+            slideIndex--;
+        } else {
+            slideIndex = backgroundImages.length - 1;
+        }
+        homeContainer.style.backgroundImage = `url(${backgroundImages[slideIndex]})`;
+    }
 
     const slogan = document.createElement('p');
     slogan.textContent = "Taco night is here.";
     slogan.classList.add('home-text');
+
+    homeContainer.appendChild(buttonL);
+    container.appendChild(homeContainer);
     homeContainer.appendChild(slogan);
+    homeContainer.appendChild(buttonR);
 }
